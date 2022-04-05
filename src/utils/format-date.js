5d89a1ea89b6b94558918e40a765535996aa9e4b
Chris Márffy <chris@lab3apps.com>
2022-04-05 18:47:17 +0100

    Abstracting code + jsdoc

--- src/utils/format-date.js
+++ src/utils/format-date.js
@@ -7,28 +7,36 @@ export function formatDate(timestamp) {
   }).format(new Date(timestamp));
 }
 
+/**
+ * Formats an ISO timestamp string into a format in the pattern:
+ * November 21, 2020, 9:17:00 AM {Timezone or Offset}
+ * @param {string} timestamp - An ISO timestamp
+ * @param {boolean} keepSourceTimeZone - Whether to return the string in the timezone 
+ * specified in the ISO offset (true), or the users local timezone (false, default)
+ * @returns 
+ */
 export function formatDateTime(timestamp, keepSourceTimeZone=false) {
+  const dateFormatOptions = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+  };
   if(keepSourceTimeZone){
     const timeStampWithNoZone = timestamp.substr(0, 19);
     const timeZoneString = timestamp.substr(19);
-    return (
-      `${new Intl.DateTimeFormat("en-US", {
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-        hour: "numeric",
-        minute: "numeric",
-        second: "numeric",
-      }).format(new Date(timeStampWithNoZone))} UCT${timeZoneString}`
-    );
+
+    //Return date without timezone + the seperated offset string
+    return `${new Intl.DateTimeFormat("en-US", dateFormatOptions).format(
+      new Date(timeStampWithNoZone)
+    )} UCT${timeZoneString}`;
   }else{
+
+    //Retur date with the users local timezone included
     return new Intl.DateTimeFormat("en-US", {
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-      hour: "numeric",
-      minute: "numeric",
-      second: "numeric",
+      ...dateFormatOptions,
       timeZoneName: "short",
     }).format(new Date(timestamp));
   }
